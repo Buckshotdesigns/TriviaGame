85cb860c29a9cc0e4bbd33d44dc02f5b49aac1de
bryanbuckus <bryanbuckus@gmail.com>
2019-02-23 17:13:13 -0500

    added styling to html to begin javascript and jquery

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+
+
+// have the game rules displayed 
+
+function startGame (){
+
+    
+
+
+
+}
+
+// start the game by clicking a button that says start
+
+// have the timer running at the top of the screen set to 30 seconds 
+
+// display question number one with four possible answers
+
+// when user clicks the answer it needs to display correct or incorrect
+
+// the correct answer will display underneath 
+
+// after answer is selected the timer will give you 5 seconds before loading next question
+
+// next question is loaded 
+
+// when game is complete show correct, incorrect and unanswered questions
+
+// have a button to reload the game 
